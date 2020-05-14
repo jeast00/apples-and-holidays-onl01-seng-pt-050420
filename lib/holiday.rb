40157734fc602ca3465fffb1ca247307bac2456a
Jonathan East <jonathan_east@ymail.com>
2020-05-14 01:23:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,6 +76,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_names = []
   holiday_hash.each do |seasons, holidays|
     holidays.each do |holiday, supply|
       binding.pry
